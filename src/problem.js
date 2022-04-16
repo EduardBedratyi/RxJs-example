283afb3ec26831137ea85ca726facec19c2ccbb2
Eduard_Bedratyi <eduard.bedratyi@jetsoftpro.com>
2022-04-17 01:39:02 +0300

    AP-1: start RxJs as analogy of JS solution

--- src/problem.js
+++ src/problem.js
@@ -1,3 +1,6 @@
+import { interval } from "rxjs";
+import { filter, map, take, scan } from "rxjs/operators";
+
 const btn = document.getElementById("interval");
 const rxjsBtn = document.getElementById("rxjs");
 const display = document.querySelector("#problem .result");
@@ -29,4 +32,25 @@ btn.addEventListener("click", () => {
       btn.disabled = false;
     }
   }, 1000);
+});
+
+rxjsBtn.addEventListener("click", () => {
+  rxjsBtn.disabled = true;
+  // setup new stream by running interval
+  interval(1000)
+    // pipe method contains operators to manipule with stream
+    .pipe(
+      take(people.length),
+      filter((v) => people[v].age >= 18),
+      map((v) => people[v].name),
+      scan((acc, v) => acc.concat(v), [])
+    )
+    // to get result use subscribe, which can accept 3 arguments
+    .subscribe(
+      (res) => {
+        display.textContent = res.join(" ");
+      },
+      null,
+      () => (rxjsBtn.disabled = false)
+    );
 });
