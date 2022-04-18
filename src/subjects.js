5951b27f5b83dfda013e27fb8be2bc2bc9fcfe32
Eduard_Bedratyi <eduard.bedratyi@jetsoftpro.com>
2022-04-18 20:09:20 +0300

    AP-4: create stream by ReplaySubject

--- src/subjects.js
+++ src/subjects.js
@@ -1,30 +1,25 @@
-import { Subject, BehaviorSubject } from "rxjs";
+import { Subject, BehaviorSubject, ReplaySubject } from "rxjs";
 // Subject is a kind of Observable, but first create "stream", then subscribe and then dispatch the event
 document.addEventListener("click", () => {
-  // const stream$ = new Subject();
-
-  // BehaviorSubject need an initial value
-  const stream$ = new BehaviorSubject("First of all");
-
-  stream$.subscribe((v) => console.log("Value: ", v));
-  // console.log("Value: ", v):
-  // Value:  First of all
-  // Value:  Hello
-  // Value:  world of
-  // Value:  RxJs
+  // such "stream" remember all values or as many values as we determine in argument starting from the last one
+  const stream$ = new ReplaySubject();
 
   stream$.next("Hello");
   stream$.next("world of ");
   stream$.next("RxJs");
 
-  // in case when subscribe places after all, initial value changes to last value
-  /*
-  stream$.next("Hello");
-  stream$.next("world of ");
-  stream$.next("RxJs"); 
-
   stream$.subscribe((v) => console.log("Value: ", v));
   // console.log("Value: ", v):
+  // Value:  Hello
+  // Value:  world of
   // Value:  RxJs
-  */
+  /******************************/
+  //const stream$ = new ReplaySubject(1);
+  // console.log("Value: ", v):
+  // Value:  RxJs
+  /******************************/
+  //const stream$ = new ReplaySubject(2);
+  // console.log("Value: ", v):
+  // Value:  world of
+  // Value:  RxJs
 });
