54ba4de07e707f09fa46a2a5fb067f9f435a8540
Eduard_Bedratyi <eduard.bedratyi@jetsoftpro.com>
2022-04-19 02:03:40 +0300

    AP-5: try scan method

--- src/operators.js
+++ src/operators.js
@@ -1,5 +1,13 @@
 import { interval } from "rxjs";
-import { map, filter, tap, take, takeLast, takeWhile } from "rxjs/operators";
+import {
+  map,
+  filter,
+  tap,
+  take,
+  takeLast,
+  takeWhile,
+  scan,
+} from "rxjs/operators";
 
 const stream$ = interval(1000).pipe(
   // "tap" operator use to call some side effects
@@ -7,10 +15,11 @@ const stream$ = interval(1000).pipe(
   // map((v) => v * 3),
   // filter((v) => v % 2 === 0),
   // "take" operator allows define quantity of elements
-  // take(5),
+  take(5),
   // without takeLast the process of taking "tap" will continue infinitely
   // takeLast(2)
-  takeWhile((v) => v < 3)
+  // takeWhile((v) => v < 3)
+  scan((acc, v) => acc + v, 0)
 );
 
 stream$.subscribe({
@@ -24,6 +33,9 @@ stream$.subscribe({
 // Tap v:  1
 // Next value:  1
 // Tap v:  2
-// Next value:  2
+// Next value:  3
 // Tap v:  3
+// Next value:  6
+// Tap v:  4
+// Next value:  10
 // Complete
