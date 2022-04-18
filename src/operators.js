ed0c029d0fd3ae8645e8225ff458dfad96e916d3
Eduard_Bedratyi <eduard.bedratyi@jetsoftpro.com>
2022-04-19 01:29:53 +0300

    AP-5: use takeLast method

--- src/operators.js
+++ src/operators.js
@@ -1,16 +1,28 @@
 import { interval } from "rxjs";
-import { map, filter, tap, take } from "rxjs/operators";
+import { map, filter, tap, take, takeLast } from "rxjs/operators";
 
 const stream$ = interval(1000).pipe(
   // "tap" operator use to call some side effects
   tap((v) => console.log("Tap v: ", v)),
-  map((v) => v * 3),
-  filter((v) => v % 2 === 0),
+  // map((v) => v * 3),
+  // filter((v) => v % 2 === 0),
   // "take" operator allows define quantity of elements
-  take(5)
+  take(5),
+  // without takeLast the process of taking "tap" will continue infinitely
+  takeLast(2)
 );
 
 stream$.subscribe({
   next: (v) => console.log("Next value: ", v),
   complete: () => console.log("Complete"),
 });
+
+// console.log("Tap v: ", v)
+// Tap v:  0
+// Tap v:  1
+// Tap v:  2
+// Tap v:  3
+// Tap v:  4
+//  Next value:  3
+//  Next value:  4
+//  Complete
