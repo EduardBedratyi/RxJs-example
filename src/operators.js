e74f091fcaa93b35ee1e7ee5046dc5ade35a2131
Eduard_Bedratyi <eduard.bedratyi@jetsoftpro.com>
2022-04-19 02:52:09 +0300

    AP-5: try switchMap

--- src/operators.js
+++ src/operators.js
@@ -8,20 +8,25 @@ import {
   takeWhile,
   scan,
   reduce,
+  switchMap,
 } from "rxjs/operators";
 
-fromEvent(document, "click").subscribe(() => {
-  const stream$ = interval(1000).pipe(
-    tap((v) => console.log("Tap v: ", v)),
-    take(5),
-    reduce((acc, v) => acc + v, 0)
-  );
-
-  stream$.subscribe({
-    next: (v) => console.log("Next: ", v),
+fromEvent(document, "click")
+  .pipe(
+    // switchMap allows change direction from one "stream" to another
+    switchMap((event) => {
+      // return new "stream"
+      return interval(1000).pipe(
+        tap((v) => console.log("Tap v: ", v)),
+        take(5),
+        reduce((acc, v) => acc + v, 0)
+      );
+    })
+  )
+  .subscribe({
+    next: (v) => console.log("Next value: ", v),
     complete: () => console.log("Complete"),
   });
-});
 
 // console.log("Tap v: ", v)
 // Tap v:  0
@@ -30,4 +35,3 @@ fromEvent(document, "click").subscribe(() => {
 // Tap v:  3
 // Tap v:  4
 // Next value:  10
-// Complete
