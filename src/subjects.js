1bf2e4790852e340a4fa33d15ba424758c827cdd
Eduard_Bedratyi <eduard.bedratyi@jetsoftpro.com>
2022-04-18 19:53:43 +0300

    AP-4: create stream by BehaviorSubject

--- src/subjects.js
+++ src/subjects.js
@@ -1,11 +1,30 @@
-import { Subject } from "rxjs";
+import { Subject, BehaviorSubject } from "rxjs";
 // Subject is a kind of Observable, but first create "stream", then subscribe and then dispatch the event
 document.addEventListener("click", () => {
-  const stream$ = new Subject();
+  // const stream$ = new Subject();
+
+  // BehaviorSubject need an initial value
+  const stream$ = new BehaviorSubject("First of all");
 
   stream$.subscribe((v) => console.log("Value: ", v));
+  // console.log("Value: ", v):
+  // Value:  First of all
+  // Value:  Hello
+  // Value:  world of
+  // Value:  RxJs
 
   stream$.next("Hello");
   stream$.next("world of ");
   stream$.next("RxJs");
+
+  // in case when subscribe places after all, initial value changes to last value
+  /*
+  stream$.next("Hello");
+  stream$.next("world of ");
+  stream$.next("RxJs"); 
+
+  stream$.subscribe((v) => console.log("Value: ", v));
+  // console.log("Value: ", v):
+  // Value:  RxJs
+  */
 });
